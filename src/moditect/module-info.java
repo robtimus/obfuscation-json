eccbee715a7d858978ab88981f5021d6bd725b06
Rob Spoor <rob@icemanx.nl>
2020-05-24 17:37:52 +0200

    Replaced automatic module name with a module descriptor

---
+++ src/moditect/module-info.java
@@ -0,0 +1,7 @@
+module com.github.robtimus.obfuscation.json {
+    requires transitive com.github.robtimus.obfuscation;
+    requires java.json;
+    requires org.slf4j;
+
+    exports com.github.robtimus.obfuscation.json;
+}
